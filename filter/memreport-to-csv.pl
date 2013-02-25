e58718029ed4e8d316d1a901ff0eec455706fb45
William Panlener <wpanlener@gmail.com>
2013-02-25 14:12:25 -0600

    Add memreport-to-csv filter

---
+++ filter/memreport-to-csv.pl
@@ -0,0 +1,80 @@
+use strict;
+use warnings;
+
+open(my $in,'mem-report.log') || die;
+
+my @match;
+my $ignore = 0;
+
+# Column Headers
+print "Cache Type" . "\t";
+print "Cache ID" . "\t";
+print "Cache Unit" . "\t";
+print " " . "\t";
+print " " . "\t";
+print " " . "\t";
+print " " . "\t";
+print " " . "\t";
+print " " . "\t";
+print " " . "\t";
+print " " . "\t";
+print " " . "\t";
+print " " . "\t";
+print " " . "\t";
+print " " . "\t";
+print " " . "\t";
+print " " . "\t";
+print " " . "\t";
+print " " . "\t";
+print " " . "\t";
+print " " . "\t";
+print " " . "\t";
+print " " . "\t";
+print " " . "\t";
+print " " . "\t";
+print " " . "\t";
+print " " . "\t";
+print " " . "\t";
+print " " . "\t";
+print " " . "\t";
+print " " . "\t";
+print " " . "\t";
+print " " . "\t";
+print " " . "\t";
+print " " . "\t";
+print " " . "\t";
+print "NoRetryAccesses" . "\t";
+print "NoRetryHits" . "\t";
+
+while (my $line = <$in>) {
+  chomp($line);
+
+  # Ignore networks and global memory segments 
+  if ($line =~ /^\[ Network/ ||
+      $line =~ /^\[ si-gm/) {
+    $ignore = 1;
+  }
+
+  if ($line =~ /^\[ si-l(\d+)-(\d+)(-(scalar|vector))?/) {
+    $ignore = 0;
+    # L1
+    if($1 == "1") {
+      print "\nl" . $1 . "\t" . "$2" . "\t" . "$4" . "\t";
+    }
+
+    # L2
+    if($1 == "2") {
+      print "\nl" . $1 . "\t" . "$2" . "\t" . " " . "\t";
+    }
+  }
+
+  if( !$ignore ) {
+    if ($line =~ /^.*=\s*(\S*)$/) {
+      print $1 . "\t";
+    }
+  }
+}
+
+close($in) || die;
+
+print @match;
