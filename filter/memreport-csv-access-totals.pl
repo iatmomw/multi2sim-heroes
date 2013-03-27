0d705a2b9b9f47425f325fb33143d189a8f160f6
William Panlener <wpanlener@gmail.com>
2013-03-27 17:00:21 -0500

    Add several filters to further automation of statistic collection

---
+++ filter/memreport-csv-access-totals.pl
@@ -0,0 +1,39 @@
+use strict;
+use warnings;
+
+my $scalar_accesses = 0;
+my $scalar_hits = 0;
+my $vector_accesses = 0;
+my $vector_hits = 0;
+my $l2_accesses = 0;
+my $l2_hits = 0;
+
+while (my $line = <>) {
+  chomp($line);
+
+  if ($line =~ /^l(1|2)\t(\d+)\t(scalar|vector|\ )?\t(\d+)\t(\d+)/) {
+    # L1
+    if($1 == "1") {
+      if($3 eq "scalar") {
+        $scalar_accesses = $scalar_accesses + $4;
+        $scalar_hits = $scalar_hits + $5;
+      }
+
+      if($3 eq "vector") {
+        $vector_accesses = $vector_accesses + $4;
+        $vector_hits = $vector_hits + $5;
+      }
+    }
+
+    # L2
+    if($1 == "2") {
+      $l2_accesses = $l2_accesses + $4;
+      $l2_hits = $l2_hits + $5;
+    }
+  }
+}
+
+print " \tAccesses\tHits\tRatio\n";
+print "Scalar\t" . $scalar_accesses . "\t" . $scalar_hits . "\t" . $scalar_hits / $scalar_accesses . "\n";
+print "Vector\t" . $vector_accesses . "\t" . $vector_hits . "\t" . $vector_hits / $vector_accesses . "\n";
+print "L2\t" . $l2_accesses . "\t" . $l2_hits . "\t" . $l2_hits / $l2_accesses . "\n";
---
+++ filter/memreport-csv-column-sort.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sort -t$'\t' -k 1,1d -k 3,3d -k 2,2n
