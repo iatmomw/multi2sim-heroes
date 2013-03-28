e7b547250787fcb5073b9771e87a9818b041af0a
William Panlener <wpanlener@gmail.com>
2013-03-27 23:42:28 -0500

    Add BitonicSort_LOCALITY and do automatic statistic gathering on benchmarks

--- batch/batch_run.sh
+++ batch/batch_run.sh
@@ -15,11 +15,14 @@ function bench {
   --si-report ${FILE_PREFIX}si_report.txt \
   --mem-report ${FILE_PREFIX}mem_report.txt \
   /usr/local/bin/m2s-bench-amdapp-2.5-si/${1}/${1} --load ${1}_Kernels.bin -q ${2} #2> ${FILE_PREFIX}log
+
+  FILTER_PREFIX="../filter"
+  echo "${1}_${TAG}" >> ./multi2sim_${TAG}/report.txt &&  ${FILTER_PREFIX}/memreport-to-stat-chain.sh ${FILTER_PREFIX} ${FILE_PREFIX}mem_report.txt >> ./multi2sim_${TAG}/report.txt && echo "" >> ./multi2sim_${TAG}/report.txt
 }
 
 TAG="LOCALITY"; SOP="--si-workgroup-scheduling-policy UserLocality"
 prep
-###( bench "BitonicSort" "-x 4096" ) &
+( bench "BitonicSort" "-x 4096" ) &
 ( bench "DCT" "-x 1024 -y 1024" ) &
 #( bench "RecursiveGaussian" "" ) &
 ###( bench "FloydWarshall" "" ) &
@@ -28,7 +31,7 @@ prep
 ##( bench "ScanLargeArrays" "" ) &
 ( bench "Reduction" "-x 1048576" ) &
 ##( bench "RadixSort" "" ) &
-##( bench "Histogram" "-x 8192 -y 8192" ) &
+( bench "Histogram" "-x 8192 -y 8192" ) &
 ( bench "BinomialOption" "-x 1024" ) &
 ##( bench "DwtHaar1D" "" ) &
 ##( bench "BinarySearch" "" ) &
