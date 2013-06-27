f266a1f22038bbfd1e919e3232e4dc938a56c639
William Panlener <wpanlener@gmail.com>
2013-06-27 15:14:12 -0700

    Add batch script for calling parser and generating matrix

---
+++ staging/gen_matrix.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+TRACE="net.trace";
+MATRIX="cct-messages.txt"
+
+function add_network {
+  perl net-trace-to-matrix.pl "${1}" < "$TRACE" >> "$MATRIX";
+}
+
+rm $MATRIX;
+
+add_network "si-net-l2-0-gm-0"; 
+add_network "perl si-net-l2-1-gm-1";
+add_network "si-net-l2-2-gm-2";
+add_network "si-net-l2-3-gm-3";
+add_network "si-net-l2-4-gm-4";
+add_network "si-net-l2-5-gm-5";
+add_network "si-net-l1-l2";
